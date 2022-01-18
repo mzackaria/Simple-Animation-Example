1c78d2fe223ae6ba8cfcd5da761ca9cf34c27b54
Zakaria MESSAI <messai.zackaria@gmail.com>
2022-01-18 11:51:07 +0100

    animation on start

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -3,10 +3,13 @@ package com.udacity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), MotionLayout.TransitionListener {
+
+    var toStartTransitionInProgress = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +23,27 @@ class DetailActivity : AppCompatActivity() {
 
         if (intent.hasExtra(EXTRA_CODE_STATUS))
             mStatusTV.isSuccess = intent.getBooleanExtra(EXTRA_CODE_STATUS, false)
+
+        motionLayout.setTransitionListener(this)
+
     }
 
     fun buttonClick(v: View) {
-        finish()
+        toStartTransitionInProgress = true
+        motionLayout.setTransitionDuration(400)
+        motionLayout.transitionToStart()
     }
 
+    override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+    }
+
+    override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+    }
+
+    override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+        if (toStartTransitionInProgress) finish()
+    }
+
+    override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+    }
 }
